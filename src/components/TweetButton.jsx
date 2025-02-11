5f14844f70ccc423e7982794de4fb0478dba042c
Hamid Rajabi Kojidi <rajabihamid75@gmail.com>
2025-02-11 13:49:47 +0330

    feat: make users provide a rating
    BREAKING CHANGE: This commit breaks compatibility with the previous version

--- src/components/TweetButton.jsx
+++ src/components/TweetButton.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import "../index.css";
 
-export function TweetButton(props) {
-    const tweetURL = `https://twitter.com/intent/tweet? text=Thank+you,+%40oluwatobiss.+Your+book+helped+me+create,+test,+ and+publish+${props.number && props.number > 1 ? props.number : "an"}+NPM+${props.number && props.number > 1 ? "packages" : "package"}.%0A%0ACreating%20NPM%20Package:%20React%20JavaScript%20G
-    uide%0A%0Ahttps%3A%2F%2Famzn.to/4aUJ3Qg`;
+function generateStarIcons(rating) {
+    const megaStars = ` 􀀀􀀀􀀀􀀀􀀀 x ${Math.round(rating / 5)}`;
+    let stars = "";
 
-    return (
+    for (let i = 0; i < rating; i++) {
+        stars += "􀀀";
+    }
+
+    return rating > 10 ? megaStars : stars;
+}
+
+export function TweetButton(props) {
+    const tweetURL = `https://twitter.com/intent/tweet?
+    text=Thank+you,+%40oluwatobiss.+Your+book+helped+me+create,+test,+
+    and+publish+${props.number && props.number > 1 ? props.number : "an"
+        }+NPM+${props.number && props.number > 1 ? "packages" : "package"
+        }.%0A%0ABook's+Rating:+${props.rating}-
+    star+rating!+${generateStarIcons(props.rating)}+%0A%0ACreating%20N
+    PM%20Package:%20React%20JavaScript%20Guide%0A%0Ahttps%3A%
+    2F%2Famzn.to/4aUJ3Qg`;
+    return props.rating ? (
         <section className="tweet-btn-container">
             <a
                 className="tweet-button"
@@ -15,5 +31,25 @@ export function TweetButton(props) {
                 Send a thank you tweet
             </a>
         </section>
+    ) : (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                marginTop: "30px",
+            }}
+        >
+            <p
+                style={{
+                    backgroundColor: "#DC3545",
+                    color: "#fff",
+                    width: "35%",
+                    borderRadius: "5px",
+                    padding: "15px 20px",
+                }}
+            >
+                􀀀 Error: Props 'rating' is missing in 'TweetButtonProps'.
+            </p>
+        </div>
     );
 }
