eb4e4b24391fd23457e1eecbe3d0a849049a2d7f
Hamid Rajabi Kojidi <rajabihamid75@gmail.com>
2025-02-11 13:34:28 +0330

    refactor: change div to section element

--- src/components/TweetButton.jsx
+++ src/components/TweetButton.jsx
@@ -5,10 +5,10 @@ export function TweetButton() {
     const tweetURL = "https://hamidrajabi.com";
     
     return (
-        <div className="tweet-btn-container">
+        <section className="tweet-btn-container">
             <a className="tweet-button" href={tweetURL} target="_blank">
                 Send a thank you tweet
             </a>
-        </div>
+        </section>
     );
 }
